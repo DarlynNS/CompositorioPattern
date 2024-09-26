ff5cd848f3afd7888b5571058cce2291a4ae4bf5
DarlynNS <darlyn0907@gmail.com>
2024-09-26 14:41:07 -0300

    Criação da classe TCriteria

--- Tcriteria.class.php
+++ Tcriteria.class.php
@@ -12,18 +12,64 @@
     //Método construtor
     function __construct(){
         $this->expressions = array();
-        $this->properties = array();
         $this->operators = array();
- }
+    }
 
- /*
- * Método Add
- * adiciona uma expressão ao critério
- * @param $expressions = expressão (objeto TExpression)
- * @param $operator = operador lógico de comparação
- */
+    /*
+    * Método Add
+    * adiciona uma expressão ao critério
+    * @param $expressions = expressão (objeto TExpression)
+    * @param $operator = operador lógico de comparação
+    */
  
     public function add(TExpression $expression, $operator = self::AND_OPERATOR) {
-        // na primeira vez, não precisamos de operador lógico para 
-}
+        // na primeira vez, não precisamos de operador lógico para concatenar
+        if(empty($this->expressions)) {
+            $operator = NULL;
+        }
+    // agrega o resultado da expressão a lista de expressões
+    $this->expressions[] = $expression;
+    $this->operators[] = $operator;
+    }
+    /* Método dump()
+    * retorna a expressão final
+    */
+    public function dump(){
+        //concatena a lista de expressões
+        if(count($this->expressions)>0) {
+            $result = '';
+            foreach($this->expressions as $i=> $expression) {
+                $operator = $this->operators[$i];
+                //concatena o operador com a respectiva expressão
+                $result .= $operator. $expression->dump(). ' ';
+            }
+            $result = trim($result);
+            return "({$result})";
+        }
+    }
+    /* Método SetPropriety
+    * define o valor de uma popriedade
+    * @param $property = propriedade
+    * @param $value = valor
+    */
+    public function setProperty($property, $value){
+        if(isset($value)){
+            $this->properties[$property] = $value;
+        }else{
+            $this->properties[$property] = $NULL;
+        }
+    }
+
+    /* Método getProperty()
+    * retorna o valor da propriedade
+    *@param $property = propriedade
+    */
+
+    public function getProperty($property) {
+        if(isset($this->properties[$property])){
+            return $this->properties[$property];
+        }
+    }
+ }
+ 
 ?>
